b1c612f52db80b1acc416fa71af4c5a8b02399f8
vc174887 <vc174887@fh-muenster.de>
2021-06-05 10:08:31 +0200

    crypto encode decode beim messaging

--- api-server/src/routes/messages.ts
+++ api-server/src/routes/messages.ts
@@ -6,6 +6,7 @@ import { User } from '../models/user';
 import { GenericDAO } from '../models/generic.dao';
 import { authService } from '../services/auth.service';
 import { checkFormPromise } from './users';
+import { cryptoService } from '../services/crypto.service';
 
 const router = express.Router();
 
@@ -28,18 +29,19 @@ router.post('', authService.expressMiddleware, async (req, res) => {
     })
     .then(() => {
       const message = {
-        to: req.body.to,
-        title: req.body.title,
-        content: req.body.content,
+        to: cryptoService.encrypt(req.body.to),//hier crypto encode
+        title: cryptoService.encrypt(req.body.title),
+        content: cryptoService.encrypt(req.body.content),
         date: req.body.date
       };
       return messageDAO.create(message);
     })
     .then(message => {
-      res.status(200).json({ results: message });
+      // res.status(200).json({ results: message });
+      res.status(200);
     })
-    .catch((message: string[]) => {
-      res.status(401).json({ message });
+    .catch((errorMessage: string[]) => {
+      res.status(401).json({ errorMessage });
     });
 });
 
@@ -49,8 +51,19 @@ router.get('', authService.expressMiddleware, async (req, res) => {
   const user = await userDAO.findOne({ id: res.locals.user.id });
   if (user) {
     const messages = await messageDAO.findAll({ to: user.name });
+    let decodedMessages;
     if (messages) {
-      res.status(200).json({ results: messages });
+      messages.forEach( ( message : Message ) => {
+        const decodedMessage = {
+          to: cryptoService.decrypt(message.to),
+          title: cryptoService.decrypt(message.title),
+          content: cryptoService.decrypt(message.content),
+          date: req.body.date
+        };
+        decodedMessages.push(decodedMessage);
+
+      })
+      res.status(200).json({ results: decodedMessages });
     }
   } else {
     res.status(404).end();
